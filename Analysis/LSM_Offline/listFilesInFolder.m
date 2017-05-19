4161f3e7ab67757dbd7767a3846c612b95f0a045
Paolo Baesso <paolo.baesso@bristol.ac.uk>
2017-05-19 01:04:48 +0100

    Sorted out Analysis folder

--- Analysis/LSM_Offline/listFilesInFolder.m
+++ Analysis/LSM_Offline/listFilesInFolder.m
@@ -2,11 +2,7 @@ function [ fileNameList ] = listFilesInFolder( myFolder )
  %% LISTFILESINFOLDER Returns a list containing the names of the image files in the folder
  %  This function searches for .tif files in the folder.
  
-    %myFolder= 'D:\LSM_Data\OldFiles\150923_TestAutotrackWithPlant';
     fileNameList = dir([myFolder '*.tif']);
     
-    %fileNameList(2).name
-    %fileNameList(2).date
-
 end
 
